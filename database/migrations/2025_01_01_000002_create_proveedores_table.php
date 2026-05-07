dcf3cbd85126dd2c9c4fbcc8d683ab40e31bddfe
sebas-1106 <acevedosebastian455@gmail.com>
2026-05-07 22:43:54 +0200

    Add foreign key constraint

--- database/migrations/2025_01_01_000002_create_proveedores_table.php
+++ database/migrations/2025_01_01_000002_create_proveedores_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
 
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('proveedores');
+        Schema::enableForeignKeyConstraints();
     }
 };
