e14ca6a034cd3daf552a50eee2ed0017a4862e95
sebas-1106 <acevedosebastian455@gmail.com>
2026-05-07 23:37:12 +0200

    Add new allowed origin for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -2,22 +2,11 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => [
+    'http://localhost:4200',
+    'https://gamerdiary-frontend.vercel.app'],
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
 
